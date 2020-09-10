da353c9b65afcc46b7e8ee5ec4b5e9b0d4fd675d
dalyIsaac <isaac.daly@outlook.com>
2020-09-10 15:47:51 +1200

    Added @unimelb/pattern-lib

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button-icon icon="chevron-right">I am a button with chevron right</button-icon>
+
+    <check-list>
+      <ol slot="list">
+        <li>
+          Check the
+          <a href="a link">degree website</a>.
+        </li>
+        <li>
+          Check the entry requirements to
+          <em>clarify</em> your eligibility for application, duplicate instructions after to demonstrate a multiline item-cms: Check the entry requirements to clarify your eligibility for application.
+        </li>
+        <li>Ensure you meet the University's language requirements.</li>
+        <li>Apply online or via the hardcopy application form.</li>
+      </ol>
+      <a slot="btn" href="/">Continue</a>
+    </check-list>
+    <logo size="sm"></logo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ButtonIcon, Logo, CheckList } from "@unimelb/pattern-lib-vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ButtonIcon,
+    Logo,
+    CheckList,
+  },
+};
 </script>
 
 <style>
+@import "https://d2glwx35mhbfwf.cloudfront.net/v8.0.1/ui.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +44,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: white;
 }
 </style>
